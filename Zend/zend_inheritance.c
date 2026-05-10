5dab30e977321705c23c35ab67fc31fb4c104734
azjezz <azjezz@protonmail.com>
2026-05-10 03:45:51 +0100

    refactor(zend): extract zend_clone_arg_info_block helper for substitution clones
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- Zend/zend_inheritance.c
+++ Zend/zend_inheritance.c
@@ -43,6 +43,8 @@ static void zend_check_generic_link_bounds(
 static void zend_substitute_trait_method_arg_info(
 		zend_function *new_fn, const zend_function *orig_fn,
 		const zend_type *bind_args, uint32_t bind_arity);
+static zend_arg_info *zend_clone_arg_info_block(
+		const zend_arg_info *orig_block, uint32_t total);
 
 /* Unresolved means that class declarations that are currently not available are needed to
  * determine whether the inheritance is valid or not. At runtime UNRESOLVED should be treated
@@ -2008,20 +2010,8 @@ static void do_inherit_property(zend_property_info *parent_info, zend_string *ke
 								uint32_t num_args = orig->op_array.num_args;
 								if (orig->op_array.fn_flags & ZEND_ACC_VARIADIC) num_args++;
 								uint32_t total = num_args + 1;
-								size_t arg_info_block = sizeof(zend_arg_info) * total;
 
-								zend_arg_info *new_arg_info = zend_arena_alloc(&CG(arena), arg_info_block);
-								memcpy(new_arg_info, orig->op_array.arg_info - 1, arg_info_block);
-
-								for (uint32_t i = 0; i < total; i++) {
-									if (new_arg_info[i].name) {
-										zend_string_addref(new_arg_info[i].name);
-									}
-
-									if (new_arg_info[i].doc_comment) {
-										zend_string_addref(new_arg_info[i].doc_comment);
-									}
-								}
+								zend_arg_info *new_arg_info = zend_clone_arg_info_block(orig->op_array.arg_info - 1, total);
 
 								uint32_t slot = (hi == ZEND_PROPERTY_HOOK_GET) ? 0 : 1;
 								new_arg_info[slot].type = sub;
@@ -2864,6 +2854,23 @@ static const zend_type_named_with_args *zend_find_trait_use_binding_by_name(
 	return NULL;
 }
 
+static zend_arg_info *zend_clone_arg_info_block(const zend_arg_info *orig_block, uint32_t total)
+{
+	zend_arg_info *new_block = zend_arena_alloc(&CG(arena), sizeof(zend_arg_info) * total);
+	memcpy(new_block, orig_block, sizeof(zend_arg_info) * total);
+	for (uint32_t i = 0; i < total; i++) {
+		if (new_block[i].name) {
+			zend_string_addref(new_block[i].name);
+		}
+
+		if (new_block[i].doc_comment) {
+			zend_string_addref(new_block[i].doc_comment);
+		}
+	}
+
+	return new_block;
+}
+
 static void zend_substitute_trait_method_arg_info(
 		zend_function *new_fn, const zend_function *orig_fn,
 		const zend_type *bind_args, uint32_t bind_arity)
@@ -2887,19 +2894,7 @@ static void zend_substitute_trait_method_arg_info(
 	}
 
 	const zend_arg_info *orig_block = has_return ? orig_op->arg_info - 1 : orig_op->arg_info;
-	zend_arg_info *new_block = zend_arena_alloc(&CG(arena), sizeof(zend_arg_info) * total);
-	memcpy(new_block, orig_block, sizeof(zend_arg_info) * total);
-
-	/* The clone shares name/doc_comment string pointers with the parent's arg_info; bump refcount
-	 * so opcache's interning pass on the parent doesn't release strings the clone still references. */
-	for (uint32_t i = 0; i < total; i++) {
-		if (new_block[i].name) {
-			zend_string_addref(new_block[i].name);
-		}
-		if (new_block[i].doc_comment) {
-			zend_string_addref(new_block[i].doc_comment);
-		}
-	}
+	zend_arg_info *new_block = zend_clone_arg_info_block(orig_block, total);
 
 	uint32_t return_slot_offset = has_return ? 1 : 0;
 
