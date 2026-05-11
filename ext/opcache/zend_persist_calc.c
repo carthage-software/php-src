14e68a009e203a943ee8afcb420f2eb8e245cee4
azjezz <azjezz@protonmail.com>
2026-05-11 02:21:52 +0100

    fix(opcache): persist NAMED_WITH_ARGS payloads nested inside union/intersection list types
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- ext/opcache/zend_persist_calc.c
+++ ext/opcache/zend_persist_calc.c
@@ -239,6 +239,11 @@ static void zend_persist_type_calc(zend_type *type)
 			continue;
 		}
 
+		if (ZEND_TYPE_HAS_NAMED_WITH_ARGS(*single_type)) {
+			zend_persist_type_calc(single_type);
+			continue;
+		}
+
 		if (ZEND_TYPE_HAS_NAME(*single_type)) {
 			zend_string *type_name = ZEND_TYPE_NAME(*single_type);
 			ADD_INTERNED_STRING(type_name);
